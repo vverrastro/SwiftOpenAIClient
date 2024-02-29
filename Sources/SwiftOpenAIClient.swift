5c2fbad7c1a767fcae13e75a46185772c46d8069
Vito Verrastro <verrastro.vito90@gmail.com>
2024-02-29 12:29:14 +0100

    added log to check system message

--- Sources/SwiftOpenAIClient.swift
+++ Sources/SwiftOpenAIClient.swift
@@ -56,10 +56,12 @@ public struct OpenAIClient {
             }
             return false
         }
+        Logger.shared.log(message: "System messagge before: \(systemMessageExists)", level: .info)
 
         if !systemMessageExists {
             conversationHistory.insert(.ChatCompletionRequestSystemMessage(.init(content: assistantPrompt, role: .system)), at: 0)
         }
+        Logger.shared.log(message: "System messagge after: \(systemMessageExists)", level: .info)
         
         Logger.shared.log(message: "Prompt: \(prompt)", level: .info)
         Logger.shared.log(message: "Assistant Prompt: \(assistantPrompt)", level: .info)
